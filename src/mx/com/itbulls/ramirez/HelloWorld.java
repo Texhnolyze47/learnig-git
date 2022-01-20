6bd893ece3aab0480263bc514fead4a8edda0243
Texhnolyze <ichigobankai136@gmail.com>
2022-01-19 21:16:53 -0600

    espacios añÃadidos

--- src/mx/com/itbulls/ramirez/HelloWorld.java
+++ src/mx/com/itbulls/ramirez/HelloWorld.java
@@ -8,22 +8,16 @@ package mx.com.itbulls.ramirez;
 
 public class HelloWorld {
 	
-	/**
-	 * The entry point to the program
-	 * @param args inputs arguments
-	 */
+
 	public static void main(String[] args) {
-		
-		/**
-		 * 
-		 * @code Printing through the console
-		 */
-		
+
 	
 		System.out.println("Hello World");
-		System.out.println("Modificación del programa");
-		System.out.println("Modificación del programa 2");
-		System.out.println("Modificación del protocolo a SSH");
+	
+
+
+
+		
 
 	
 	}
