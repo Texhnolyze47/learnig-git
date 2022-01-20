4942e4ed17a6fcc6adc071c0c8ba634843444d7b
Texhnolyze <ichigobankai136@gmail.com>
2022-01-19 21:28:17 -0600

    falto un commit

--- src/mx/com/itbulls/ramirez/HelloWorld.java
+++ src/mx/com/itbulls/ramirez/HelloWorld.java
@@ -13,23 +13,12 @@ public class HelloWorld {
 	 * @param args inputs arguments
 	 */
 	public static void main(String[] args) {
-		
-		/**
-		 * 
-		 * @code Printing through the console
-		 */
-		
-	
+
 		System.out.println("Hello World");
-		System.out.println("Modificacion del programa");
-		System.out.println("Modificacion del programa 2");
-		System.out.println("Modificacion del protocolo a SSH");
+
 
 		
-		//TODO add more code
-		
-		/**
-		 * @return is a void method 
-		 */
+
+	
 	}
 }
